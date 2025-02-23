f57a28ade4a6cb4d1ec0ca13266f0a62af2eac83
Fausta Pavalkyte <B00165884@mytudublin.ie>
2025-02-23 18:05:42 +0000

    review css

--- reviews/reviews.php
+++ reviews/reviews.php
@@ -1,12 +1,15 @@
+<link rel="stylesheet" href="css/reviews.css">
 <?php
     $reviewData = file('reviewData.txt', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
 
-    echo "<div class='top-nav'>
-            <a href='../index.php'>Home</a>
-            <a href='reviews.php'>Reviews</a>
-            <a href='../products/products.php'>Products</a>
-            <a href='../login/login.php'>Login</a>
-        </div>";
+    echo "<nav>
+            <div class='top-nav'>
+                <a href='../index.php'>Home</a>
+                <a href='reviews.php'>Reviews</a>
+                <a href='../products/products.php'>Products</a>
+                <a href='../login/login.php'>Login</a>
+            </div>
+        </nav>";
 
     echo "<h1>Product Reviews</h1>";
 
@@ -15,6 +18,7 @@
     } else {
         foreach ($reviewData as $index => $review) {
             list($category, $pname, $qualityRating, $priceRating, $averageRating, $reviewText, $url) = explode('|', $review);
+            echo "<div class='box'>";
             echo "<div class='thumbnail'>
                     <img src='PlaceHolder.png' alt='Placeholder product image'>
                     </div>";
@@ -27,5 +31,7 @@
             echo "<strong>Overall Rating:</strong> $averageRating/5<br>";
             echo "<a href='productReview.php?id=$index'>Read Full Review</a><br><br>";
             echo "</div>";
+            echo "</div>";
+
         }
     }
